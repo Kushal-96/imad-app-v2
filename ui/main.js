f8e4ccea9931fba9db21604707cd9cd458cdf9f8
Kushal-96 <smartybasu@gmail.com>
2017-02-04 13:16:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('one');
 element.innerHTML='new value';
 var img=document.getElementById('two');
-img.onClick=function(){
+img.onclick=function(){
     img.style.marginLeft='100px';
 };
