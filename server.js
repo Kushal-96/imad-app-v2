a440e2b05de627499c5e703b7da988c77dac651c
Kushal-96 <smartybasu@gmail.com>
2017-02-04 11:09:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ var articles={
 },
     articleTwo: {
         title: 'Article-Two |Basudeb Mitra',
-    heading: 'Article-two',
-    date: '4th February, 2017',
-    content:`<p>
+        heading: 'Article-two',
+        date: '4th February, 2017',
+        content:`<p>
                 Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.
             </p>
             <p>
@@ -34,9 +34,9 @@ var articles={
             </p>`},
     articleThree: {
         title: 'Article-Three |Basudeb Mitra',
-    heading: 'Article-three',
-    date: '4th February, 2017',
-    content:`<p>
+        heading: 'Article-three',
+        date: '4th February, 2017',
+        content:`<p>
                 Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.Today, I learned to add web-pages to an existing web-server.
             </p>
             <p>
@@ -99,13 +99,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne)); 
+   res.send(createTemplate(articles.articleOne)); 
 });
 app.get('/article-two',function(req,res){
-    res.send(createTemplate(articleTwo)); 
+    res.send(createTemplate(articles.articleTwo)); 
 });
 app.get('/article-three',function(req,res){
-    res.send(createTemplate(articleThree)); 
+    res.send(createTemplate(articles.articleThree)); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
