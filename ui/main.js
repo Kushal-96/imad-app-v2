e6ca0cdc9723175f88e78d6a32540c53de163f65
Kushal-96 <smartybasu@gmail.com>
2017-02-04 21:34:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,16 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
-  counter+=1;
-  var span=document.getElementById('qw');
-  span.innerHTML=counter.toString();
+    var req=new XMLHttpRequest();
+    req.onreadystatechange=function(){
+        if(req.readyState===XMLHttpRequest.DONE){
+            if(req.status===200){
+                var counter=req.responseText;
+                var span=document.getElementById('qw');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    
+  
 };
