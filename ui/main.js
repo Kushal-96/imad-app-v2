0ce2250c3d89f2703e3e79c2b2fbe7325f2a6df8
Kushal-96 <smartybasu@gmail.com>
2017-02-06 19:37:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,17 @@ button.onclick=function(){
                 span.innerHTML=counter.toString();
             }
         }
-    }
+    };
     
   req.open('GET','http://kushal-96.imad.hasura-app.io/counter',true);
   req.send();
 };
+var but=document.getElementById('submit');
+but.onclick=function(){
+    var rq=new XMLHttpRequest();
+    rq.onreadystatechange=function(){
+        if(rq.readyState===XMLHttpRequest.DONE && rq.status===200){
+            
+        }
+    }
+}
