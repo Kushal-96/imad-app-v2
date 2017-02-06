4b65435fae236b638fa5adca5922f4fb5e0b4678
Kushal-96 <smartybasu@gmail.com>
2017-02-06 20:45:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,21 @@ but.onclick=function(){
     var rq=new XMLHttpRequest();
     rq.onreadystatechange=function(){
         if(rq.readyState===XMLHttpRequest.DONE && rq.status===200){
+            var name=rq.responseText;
+            name=JSON.parse('name');
+            var l=document.getElementById('list');
+            var st='';
+            for(var i=0;i<l.length;i++){
+                st+='<li>'+l[i]+'</li>';
+                
+            }
+            var qw=document.getElementById('list');
+            qw.innerHTML=qw;
             
         }
-    }
-}
+    };
+
+    var nameInp=document.getElementById('name');
+    var name=nameInp.value;
+    request.open(GET,"www.")
+};
