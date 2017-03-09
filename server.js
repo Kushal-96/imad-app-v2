4c71f96622fe0a6655b7bc5038d68f4da8a2f369
Kushal-96 <smartybasu@gmail.com>
2017-03-09 13:59:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool= require('pg').Pool;
 var config={
     user:'kushal-96',
     database:'kushal-96',
-    host:'db.imad.hasura-app',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB-PASSWORD
 };
