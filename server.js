ef38f3988dff9b3c2087421cb20bde302881211d
Kushal-96 <smartybasu@gmail.com>
2017-03-09 13:59:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config={
     database:'kushal-96',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB-PASSWORD
+    password:process.env.DB_PASSWORD
 };
 
 var app = express();
